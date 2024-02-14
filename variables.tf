ab0532b37dd195504358bb8edb4659edd2d5cd8e
Jonathan Gomes <jonathan.gomess1@gmail.com>
2024-02-14 16:29:24 -0300

    remove os arquivos

--- variables.tf
+++
@@ -1,9 +0,0 @@
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-variable "cluster_name" {
-  type = string
-  default = "fiap-lanches-eks-${random_string.suffix.result}"
-}
--- variables.tfvars
+++
@@ -1,4 +0,0 @@
-variable "cluster_name" {
-  type = string
-  default = "fiap-lanches-eks-${random_string.suffix.result}"
-}
