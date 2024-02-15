15017dab6553f39f2ec25292a7844e91793bcb69
Jonathan Gomes <jonathan.gomess1@gmail.com>
2024-02-15 16:22:34 -0300

    altera token

--- helm_release.tf
+++ helm_release.tf
@@ -1,20 +1,20 @@
-# # Copyright (c) HashiCorp, Inc.
-# # SPDX-License-Identifier: MPL-2.0
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       args        = ["eks", "get-token", "--cluster-name", "education-eks-C2s7VqWP"]
-#       command     = "aws"
-#     }
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", "education-eks-C2s7VqWP"]
+      command     = "aws"
+    }
+  }
+}
 
-# resource "helm_release" "fiap-lanches" {
-#   name = "fiap-lanches"
+resource "helm_release" "fiap-lanches" {
+  name = "fiap-lanches"
 
-#   chart = "https://github.com/jonathansantoss/fiap-lanches-helm/releases/download/fiap-lanches-0.2.0/fiap-lanches-0.2.0.tgz"
-# }
+  chart = "https://github.com/jonathansantoss/fiap-lanches-helm/releases/download/fiap-lanches-0.2.0/fiap-lanches-0.2.0.tgz"
+}
