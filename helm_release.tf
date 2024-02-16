c8f9761ebac70fb4b05cd17c9f1664941960466c
Jonathan Gomes <jonathan.gomess1@gmail.com>
2024-02-15 22:41:10 -0300

    altera a versão do cluster

--- helm_release.tf
+++ helm_release.tf
@@ -1,14 +1,14 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-data "aws_eks_cluster" "cluster" {
-  name = "fiap-lanches-eks-cWTzWOQb"
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = "fiap-lanches-eks-cWTzWOQb"
+# }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    host                   = "fiap-lanches-eks-cWTzWOQb".endpoint
+    cluster_ca_certificate = base64decode("fiap-lanches-eks-cWTzWOQb".certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", "fiap-lanches-eks-cWTzWOQb"]
