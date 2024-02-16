44dd6211510ee17a3db204e3b737f62a04ed836b
Jonathan Gomes <jonathan.gomess1@gmail.com>
2024-02-16 01:18:31 -0300

    altera a versão do cluster

--- helm_release.tf
+++ helm_release.tf
@@ -3,16 +3,19 @@
 
 
 provider "helm" {
-  kubernetes {
-    # host                   = data.aws_eks_cluster.cluster.endpoint
-    host                   = module.eks.cluster_endpoint
-    # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", "fiap-lanches-eks-cWTzWOQb"]
-      command     = "aws"
-    }
+  # kubernetes {
+  #   # host                   = data.aws_eks_cluster.cluster.endpoint
+  #   host                   = module.eks.cluster_endpoint
+  #   # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  #   exec {
+  #     api_version = "client.authentication.k8s.io/v1beta1"
+  #     args        = ["eks", "get-token", "--cluster-name", "fiap-lanches-eks-cWTzWOQb"]
+  #     command     = "aws"
+  #   }
+  # }
+    kubernetes {
+    config_path = "~/.kube/config"
   }
 }
 
